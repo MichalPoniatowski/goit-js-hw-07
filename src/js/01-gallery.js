8ee46c7f8b2b6473563a27f9903e359ac6ef855e
MichalPoniatowski <m.poniatowski87@gmail.com>
2023-04-04 16:38:08 +0200

    Second js done

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,27 +1,6 @@
 import { galleryItems } from "./gallery-items.js";
-// Change code below this line
-
-// console.log(galleryItems);
 
 const galleryEL = document.querySelector(".gallery");
-
-// const item = galleryItems[0].original;
-
-// console.log(item);
-
-// galleryItems.forEach((item) => {
-//   let srcValue = item.preview;
-//   let altValue = item.description;
-
-//   const li = document.createElement("li");
-//   galleryEL.append(li);
-//   const img = document.createElement("img");
-//   li.append(img);
-//   img.setAttribute("src", srcValue);
-//   img.setAttribute("alt", altValue);
-//   img.classList.add("gallery__image");
-//   //   img.setAttribute("width", "100px");
-// });
 
 galleryItems.forEach((item) => {
   galleryEL.insertAdjacentHTML(
@@ -29,18 +8,23 @@ galleryItems.forEach((item) => {
     `
     <div class="gallery__item">
      <a class="gallery__link" href="${item.original}">
-       <img class="gallery__image" src="${item.preview}" alt="${item.description} 
+       <img class="gallery__image" src="${item.preview}" alt="${item.description}" 
        data-source="${item.original}"/>
      </a>
     </div>`
   );
 });
 
-galleryEL.addEventListener("click", (event) => {
+document.querySelector(".gallery").onclick = (event) => {
   event.preventDefault();
   const instance = basicLightbox.create(`
-    <img src="${event.target.dataset.source}" width="800" height="600">
-`);
-
+		<img src="${event.target.dataset.source}">
+	`);
   instance.show();
-});
+
+  window.addEventListener("keydown", (event) => {
+    if (event.key === "Escape") {
+      instance.close();
+    }
+  });
+};
